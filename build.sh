25d72a513e85f16f3714860e8985609266e32567
RajaSudharsan Adhikesavan <sur5an@yahoo.com>
2020-05-31 20:44:30 -0700

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo docker build -t notifier .
 sudo docker rm notifier
 touch documents.db
-sudo docker run --name notifier -v documents.db:documents.db --env SLACK_API_TOKEN=pass -d -t notifier
+sudo docker run --name notifier -v documents.db:/var/notification/documents.db --env SLACK_API_TOKEN=pass -d -t notifier
